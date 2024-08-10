7781261666646fc3d3aa737c0991fc76cb84fc2a
mit-27 <meetsuthar64@gmail.com>
2024-08-10 03:34:36 -0400

    🎨 Dashboard Skeleton

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,28 +1,13 @@
-"use client"
 
 import { api } from '@/trpc/client'
 import React, { useState } from 'react'
 
 const TodoPage = () => {
 
-    const todos = api.todo.getTodos.useQuery();
-    const addTodo = api.todo.addTodo.useMutation({
-      onSettled: () => {
-        todos.refetch();
-      }
-    });
-    const [todo,setTodo] = useState<string>('')
-
   return (
     <div className='flex justify-center items-center min-h-full'>
       <div>
-        {JSON.stringify(todos?.data)}
-      </div>
-      <div>
-        <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)} />
-        <button onClick={() => {
-          addTodo.mutate(todo);
-        }}>Add</button>
+          <h1>Hello Main Page</h1>
       </div>
     </div>
   )
